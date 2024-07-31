6c15a18f7a1b5d2f58c2aa8bab337a8f0e643fbd
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2024-07-31 13:20:07 +0200

    tests

--- tests/netedit/network/lanes/inspect_selected/changeright_dialog/rail_urban/test.py
+++ tests/netedit/network/lanes/inspect_selected/changeright_dialog/rail_urban/test.py
@@ -28,11 +28,17 @@ import neteditTestFunctions as netedit  # noqa
 # Open netedit
 neteditProcess, referencePosition = netedit.setupAndStart(neteditTestRoot)
 
-# go to inspect mode
-netedit.inspectMode()
-
 # toggle select lanes
 netedit.changeEditMode(netedit.attrs.modes.network.selectLane)
+
+# go to select mode
+netedit.selectMode()
+
+# select all using invert
+netedit.selectionInvert()
+
+# go to inspect mode
+netedit.inspectMode()
 
 # inspect edge
 netedit.leftClick(referencePosition, netedit.positions.network.edge.leftBot)
