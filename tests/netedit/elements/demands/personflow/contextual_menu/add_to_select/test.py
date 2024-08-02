f66d0053fddde7305728fd80cc5b34c915e7b712
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2024-08-02 16:03:49 +0200

    tests

--- tests/netedit/elements/demands/personflow/contextual_menu/add_to_select/test.py
+++ tests/netedit/elements/demands/personflow/contextual_menu/add_to_select/test.py
@@ -31,24 +31,24 @@ neteditProcess, referencePosition = netedit.setupAndStart(neteditTestRoot)
 # go to demand mode
 netedit.supermodeDemand()
 
-# go to vehicle mode
-netedit.vehicleMode()
+# go to person mode
+netedit.personMode()
 
-# select trip over junctions
-netedit.changeElement("trip (from-to junctions)")
+# change Person
+netedit.changeElement("personFlow")
 
-# create trip
-netedit.leftClick(referencePosition, netedit.positions.elements.junction0)
-netedit.leftClick(referencePosition, netedit.positions.elements.junction3)
+# create person using three edges
+netedit.leftClick(referencePosition, netedit.positions.elements.edge0)
+netedit.leftClick(referencePosition, netedit.positions.elements.edge2)
 
-# press enter to create trip
+# press enter to create person
 netedit.typeEnter()
 
 # go to inspect mode
 netedit.inspectMode()
 
 # add to selected
-netedit.contextualMenuOperation(referencePosition, netedit.positions.elements.demands.vehicleJunction,
+netedit.contextualMenuOperation(referencePosition, netedit.positions.elements.demands.person,
                                 netedit.contextualMenu.vehicles.addToSelected)
 
 # delete junction
